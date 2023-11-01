52752a574b26ed76c66a56328058a6fe2cecf8e8
sandeepbandi924 <bandisandeep1374@gmail.com>
2023-11-01 15:02:57 +0530

    pipeline and app.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+import pickle
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
@@ -16,7 +17,7 @@ def save_object(file_path , obj):
       os.makedirs(dir_path,exist_ok=True)
 
       with open(file_path,'wb') as file_obj:
-         dill.dump(obj, file_obj)
+         pickle.dump(obj, file_obj)
 
    except Exception as e:
       raise CustomException(e,sys)
@@ -47,6 +48,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,params):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
